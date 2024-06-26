8bd730d451cd4ed3789065e797836ee2a9b7a7ce
Priyanshu <priyanshukashyap844@gmail.com>
2024-06-26 13:50:16 +0530

    updating form section

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -15,6 +15,7 @@ const Contact = () => {
                 <input type="text" placeholder='Enter Your Name' />
                 <input type="email" placeholder='Enter Your Email'  />
                 <input type="text" placeholder='Enter your message' class="message" />
+                <button>Send Message</button>
             </form>
         </div>
         </div>
